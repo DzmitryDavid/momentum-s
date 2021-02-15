6ee80baf2a1ba10f950697a4e08abcbc7929b682
Dzmitry Davidovich <Dzmitrydavidovich89@gmail.com>
2021-02-15 16:56:45 +0300

    fix gradient

--- src/js/greeting.js
+++ src/js/greeting.js
@@ -1,34 +1,38 @@
-const morningImg = ['background-image: linear-gradient(to right, #ed6ea0 0%, #ec8c69 100%);',
-    'background-image: linear-gradient(to top, #09203f 0%, #537895 100%);',
-    'background-image: linear-gradient(to top, #feada6 0%, #f5efef 100%);',
-    'background-image: linear-gradient(120deg, #f6d365 0%, #fda085 100%);',
-    'background-image: linear-gradient(to right, #eea2a2 0%, #bbc1bf 19%, #57c6e1 42%, #b49fda 79%, #7ac5d8 100%);',
-    'background-image: linear-gradient(to top, #dfe9f3 0%, white 100%);'];
+const morningImg = [
+    'linear-gradient(to right, #ed6ea0 0%, #ec8c69 100%)',
+    'linear-gradient(to top, #09203f 0%, #537895 100%)',
+    'linear-gradient(to top, #feada6 0%, #f5efef 100%)',
+    'linear-gradient(120deg, #f6d365 0%, #fda085 100%)',
+    'linear-gradient(to right, #eea2a2 0%, #bbc1bf 19%, #57c6e1 42%, #b49fda 79%, #7ac5d8 100%)',
+    'linear-gradient(to top, #dfe9f3 0%, white 100%)'];
 
-const dayImg = ['background-image: linear-gradient(to top, #50cc7f 0%, #f5d100 100%);',
-    'background-image: linear-gradient(-225deg, #5D9FFF 0%, #B8DCFF 48%, #6BBBFF 100%);',
-    'background-image: linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%);',
-    'background-image: linear-gradient(to top, #209cff 0%, #68e0cf 100%);',
-    'background-image: linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%);',
-    'background-image: linear-gradient(120deg, #d4fc79 0%, #96e6a1 100%);',
-    'background-image: linear-gradient(-225deg, #FF3CAC 0%, #562B7C 52%, #2B86C5 100%);']
+const dayImg = [
+    'linear-gradient(to top, #50cc7f 0%, #f5d100 100%)',
+    'linear-gradient(-225deg, #5D9FFF 0%, #B8DCFF 48%, #6BBBFF 100%)',
+    'linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%)',
+    'linear-gradient(to top, #209cff 0%, #68e0cf 100%)',
+    'linear-gradient(to top, #fad0c4 0%, #ffd1ff 100%)',
+    'linear-gradient(120deg, #d4fc79 0%, #96e6a1 100%)',
+    'linear-gradient(-225deg, #FF3CAC 0%, #562B7C 52%, #2B86C5 100%)']
 
-const eveningImg = ['background-image: linear-gradient(to top, #88d3ce 0%, #6e45e2 100%);',
-    'background-image: linear-gradient(to top, #a18cd1 0%, #fbc2eb 100%);',
-    'background-image: linear-gradient(120deg, #e0c3fc 0%, #8ec5fc 100%);',
-    'background: linear-gradient(to bottom, rgba(255,255,255,0.15) 0%, rgba(0,0,0,0.15) 100%), radial-gradient(at top center, rgba(255,255,255,0.40) 0%, rgba(0,0,0,0.40) 120%) #989898;',
-    'background-image: linear-gradient(to right, #ff8177 0%, #ff867a 0%, #ff8c7f 21%, #f99185 52%, #cf556c 78%, #b12a5b 100%);',
-    'background-image: linear-gradient(120deg, #a6c0fe 0%, #f68084 100%);',
-    'background-image: linear-gradient(to top, #505285 0%, #585e92 12%, #65689f 25%, #7474b0 37%, #7e7ebb 50%, #8389c7 62%, #9795d4 75%, #a2a1dc 87%, #b5aee4 100%);']
+const eveningImg = [
+    'linear-gradient(to top, #88d3ce 0%, #6e45e2 100%)',
+    'linear-gradient(to top, #a18cd1 0%, #fbc2eb 100%)',
+    'linear-gradient(120deg, #e0c3fc 0%, #8ec5fc 100%)',
+    'linear-gradient(to bottom, rgba(255,255,255,0.15) 0%, rgba(0,0,0,0.15) 100%), radial-gradient(at top center, rgba(255,255,255,0.40) 0%, rgba(0,0,0,0.40) 120%) #989898;',
+    'linear-gradient(to right, #ff8177 0%, #ff867a 0%, #ff8c7f 21%, #f99185 52%, #cf556c 78%, #b12a5b 100%)',
+    'linear-gradient(120deg, #a6c0fe 0%, #f68084 100%)',
+    'linear-gradient(to top, #505285 0%, #585e92 12%, #65689f 25%, #7474b0 37%, #7e7ebb 50%, #8389c7 62%, #9795d4 75%, #a2a1dc 87%, #b5aee4 100%)']
 
-const nightImg = ['background-image: linear-gradient(to top, #09203f 0%, #537895 100%);',
-    'background-image: linear-gradient(to top, #30cfd0 0%, #330867 100%);',
-    'background-image: linear-gradient(to top, #1e3c72 0%, #1e3c72 1%, #2a5298 100%);',
-    'background-image: linear-gradient(to top, #09203f 0%, #537895 100%);',
-    'background-image: linear-gradient(to top, #30cfd0 0%, #330867 100%);',
-    'background-image: linear-gradient(135deg, #667eea 0%, #764ba2 100%);',
-    'background-image: linear-gradient(-225deg, #A8BFFF 0%, #884D80 100%);',
-    'background-image: linear-gradient(-225deg, #473B7B 0%, #3584A7 51%, #30D2BE 100%);'
+const nightImg = [
+    'linear-gradient(to top, #09203f 0%, #537895 100%)',
+    'linear-gradient(to top, #30cfd0 0%, #330867 100%)',
+    'linear-gradient(to top, #1e3c72 0%, #1e3c72 1%, #2a5298 100%)',
+    'linear-gradient(to top, #09203f 0%, #537895 100%)',
+    'linear-gradient(to top, #30cfd0 0%, #330867 100%)',
+    'linear-gradient(135deg, #667eea 0%, #764ba2 100%)',
+    'linear-gradient(-225deg, #A8BFFF 0%, #884D80 100%)',
+    'linear-gradient(-225deg, #473B7B 0%, #3584A7 51%, #30D2BE 100%)'
 ];
 
 const nameInput = document.querySelector('.namein');
@@ -89,7 +93,7 @@ const getFocus = () => {
 getFocus()
 
 
-const setBg = (src) => document.body.style = src;
+const setBg = (src) => document.body.style.background = src ;
 
 const setGreeting = () => {
     let today = new Date();
